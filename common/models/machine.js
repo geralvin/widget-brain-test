03d7910504a90b9e8a5d2b0624fe4a8befc4ddaa
Geralvin Maheswara <geralvin.maheswara@ovo.id>
2019-11-05 00:29:00 +0700

    finishing upload

--- common/models/machine.js
+++ common/models/machine.js
@@ -1,32 +1,44 @@
 'use strict';
 
 const ExcelJs = require('exceljs');
-const workbook = new ExcelJs.Workbook();
 
 module.exports = function(Machine) {
 
   Machine.upload = async (req, res) => {
+    const workbook = new ExcelJs.Workbook();
+
     try {
       const buf = Buffer(req.files.file.data);
       workbook.xlsx.load(buf)
       .then(() => {
         const worksheet = workbook.getWorksheet(1);
-        worksheet.eachRow({includeEmpty: false}, function(row, rowNumber) {
+        worksheet.eachRow({includeEmpty: false}, async function(row, rowNumber) {
           if (rowNumber !== 1) {
             // proceed data
             const [_, machine, attribute, reading] = row.values;
-            Machine.upsertWithWhere({where: { machine }}, { machine, attribute, reading });
+            const record = await Machine.findOne({ where: { machine, attribute }});
+
+            if (record) {
+              // replace if record is found
+              await Machine.replaceById(record.id, { machine, attribute, reading });
+            } else {
+              // otherwise insert new record
+              await Machine.upsert({ machine, attribute, reading });
+            }
           }
         });
       });
 
-    return '';
+      return res.status(201);
     } catch(err) {
       throw err;
     }
   };
 
   Machine.remoteMethod('upload', {
-    accepts: {arg: 'req', type: 'object', http: { source: 'req'}}
+    accepts: [
+      {arg: 'req', type: 'object', http: { source: 'req'}},
+      {arg: 'res', type: 'object', http: { source: 'res'}},
+    ]
   });
 };
