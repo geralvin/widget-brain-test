f716499075b03ecf776f034bbab24ac61de0f722
Geralvin Maheswara <geralvin.maheswara@ovo.id>
2019-11-05 00:38:02 +0700

    add download remote method /api/machines/download [GET]

--- common/models/machine.js
+++ common/models/machine.js
@@ -3,6 +3,9 @@
 const ExcelJs = require('exceljs');
 
 module.exports = function(Machine) {
+  // Machine.disableRemoteMethodByName('deleteById', true);
+  // Machine.disableRemoteMethodByName('create', true);
+  // Machine.disableRemoteMethodByName('upsert', true);
 
   Machine.upload = async (req, res) => {
     const workbook = new ExcelJs.Workbook();
@@ -35,10 +38,40 @@ module.exports = function(Machine) {
     }
   };
 
+  Machine.download = async (req, res) => {
+    const workbook = new ExcelJs.Workbook();
+    const records = await Machine.find({ order: 'attribute DESC', fields: { id: false }});
+    
+    const worksheet = workbook.addWorksheet('Sheet');
+
+    worksheet.columns = [
+      { header: 'Machine', key: 'machine', width: '10'},
+      { header: 'Attribute', key: 'attribute', width: '10'},
+      { header: 'Reading', key: 'reading', width: '10', type: 'decimal'}
+    ];
+
+    records.forEach(({ machine, attribute, reading }) => {
+      // addRow and parse reading to number
+      worksheet.addRow({ machine, attribute, reading: parseFloat(reading.replace(/,/g, '')) });
+    });
+
+    res.attachment('download.xlsx');
+    await workbook.xlsx.write(res);
+    res.end();
+  }
+
   Machine.remoteMethod('upload', {
     accepts: [
       {arg: 'req', type: 'object', http: { source: 'req'}},
       {arg: 'res', type: 'object', http: { source: 'res'}},
     ]
+  });
+
+  Machine.remoteMethod('download', {
+    accepts: [
+      {arg: 'req', type: 'object', http: { source: 'req'}},
+      {arg: 'res', type: 'object', http: { source: 'res'}},
+    ],
+    http: {'verb': 'get'}
   });
 };
